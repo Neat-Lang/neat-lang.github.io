17cafde551373991afa3b896220b79846da44ead
FeepingCreature <feepingcreature@gmail.com>
2022-06-04 17:03:43 +0200

    Update docs to neat-lang/neat v0.1.4-3-g0f357b0

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -e
+rm -rf ../neat/doc/build/
+sphinx-build ../neat/doc/sphinx/ ../neat/doc/build
+cp -R ../neat/doc/build/* .
+git add .
+git commit -m "Update docs to neat-lang/neat $(git -C ../neat describe)"
